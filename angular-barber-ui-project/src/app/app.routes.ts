cc7958d8dfebd4e3416c0f6f0cb097a1bb79ed72
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-01-08 10:11:43 +0300

    created home components

--- angular-barber-ui-project/src/app/app.routes.ts
+++ angular-barber-ui-project/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+  {
+    path:'', component:HomeComponent
+  },
   {
     path:'admin', component:AdminComponent
   }
