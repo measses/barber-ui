787050dff8bd263665a5138a4777e830136cee23
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-01-08 12:17:30 +0300

    created sidebar, topbar and footer component

--- angular-barber-ui-project/src/app/admin/admin.component.ts
+++ angular-barber-ui-project/src/app/admin/admin.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { TopbarComponent } from './layouts/topbar/topbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, TopbarComponent, FooterComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
---
+++ angular-barber-ui-project/src/app/admin/layouts/topbar/topbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss'
+})
+export class TopbarComponent {
+
+}
