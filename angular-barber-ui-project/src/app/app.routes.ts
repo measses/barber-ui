bcb7d345505d131358fbea9943744f5df5afe1fe
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-01-08 10:08:24 +0300

    added to admin component

--- angular-barber-ui-project/src/app/app.routes.ts
+++ angular-barber-ui-project/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'admin', component:AdminComponent
+  }
+];
